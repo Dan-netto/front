763ced370820a70e5e7a1b207432ef104c72af28
daniel <daniel.netto@prodeb.ba.gov.br>
2025-11-05 23:51:44 -0300

    voltando dependências

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   plugins: {
     'postcss-import': {},
-    'postcss-nesting': {},
-    '@tailwindcss/postcss': {}, // ✅ Novo plugin oficial do Tailwind 4+
+    'tailwindcss/nesting': {}, // Or just 'tailwindcss'
+    tailwindcss: {},
     autoprefixer: {},
   },
 };
